51a4ec151d7284adf3b67191b1f70c8cf01b0b7a
Tim <tim.lorent@icloud.com>
2017-09-19 15:53:37 +0200

    Modified hero and headline section.

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -3,7 +3,8 @@ postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
-cssImport = require('postcss-import');
+cssImport = require('postcss-import'),
+mixins = require('postcss-mixins');
 
 gulp.task('styles', function() {
   return gulp.src('./app/assets/styles/styles.css')
@@ -11,6 +12,10 @@ gulp.task('styles', function() {
     // The first pipe allows the CSS to run through post-css filters
     // the postcss function requires an array of postcss filters that you can use
     // such as CSS variables, nesting, autoprefixes
-    .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+    .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
+    .on('error', function(errorInfo) {
+      console.log(errorInfo.toString());
+      this.emit('end');
+    })
     .pipe(gulp.dest('./app/temp/styles'));
 });
